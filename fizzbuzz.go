bd99ded00059c620b60f7d151ba6d30e14dc80cf
Raphael Cottier <github.zing@dfgh.net>
2023-03-02 16:49:34 +0100

    Another example

--- fizzbuzz.go
+++ fizzbuzz.go
@@ -4,16 +4,23 @@ import "fmt"
 
 func main() {
 	for i := 1; i <= 100; i++ {
-		fmt.Print(i)
-		fmt.Print(": ")
-		if i%3 == 0 && i%5 == 0 {
-			fmt.Println("FizzBuzz")
-		} else if i%3 == 0 {
-			fmt.Println("Fizz")
-		} else if i%5 == 0 {
-			fmt.Println("Buzz")
-		} else {
-			fmt.Println(i)
+		fmt.Print(i, ": ")
+		fizzy := false
+
+		if i%3 == 0 {
+			fmt.Print("Fizz")
+			fizzy = true
 		}
+
+		if i%5 == 0 {
+			fmt.Print("Buzz")
+			fizzy = true
+		}
+
+		if !fizzy {
+			fmt.Print(i)
+		}
+
+		fmt.Println()
 	}
 }
